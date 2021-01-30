816d2cbb15410450ed83ff8a77497823807e8d68
vagrant <answer.is.near.2@gmail.com>
2021-01-30 23:05:46 +0900

    (add)routing

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,5 +1,15 @@
 class MemosController < ApplicationController
 	def index
-		
+		@memo = Memo.new
+	end
+
+	def create
+		@memo = Memo.new(memo_params)
+	end
+
+	def delete
+		@memo = Memo.find(params[:id])
+		@memo.destroy
+		redirect_to memos_path
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "memos#index"
+  resources :memos,except:[:destroy,:new]
+  delete "memos" => "memos#delete"
+  post "memos" => "memos#create"
 end
